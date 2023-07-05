91fdd052c7ef818da7b4e892b524a95bdeae965f
DenerCampos <denerjcampos@gmail.com>
2023-07-05 11:15:12 -0300

    feat(data-base): SP-17 - refaz as migrations para organizar

---
+++ db/migrations/1688566343801-CreateAllTables.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAllTables1688566343801 implements MigrationInterface {
+  name = 'CreateAllTables1688566343801';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`store\` (\`id\` varchar(36) NOT NULL, \`name\` varchar(255) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`group\` (\`id\` varchar(36) NOT NULL, \`name\` varchar(255) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`item\` (\`id\` varchar(36) NOT NULL, \`code\` varchar(255) NOT NULL, \`name\` varchar(255) NOT NULL, \`quantity\` float NOT NULL, \`unit\` varchar(255) NOT NULL, \`value\` decimal(10,2) NOT NULL, \`total\` decimal(10,2) NOT NULL, \`purchaseDate\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`group_id\` varchar(36) NULL, \`coupon_id\` varchar(36) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`payment\` (\`id\` varchar(36) NOT NULL, \`name\` varchar(255) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`coupon\` (\`id\` varchar(36) NOT NULL, \`number\` varchar(255) NOT NULL, \`url\` varchar(255) NULL, \`date\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`store_id\` varchar(36) NULL, \`payment_id\` varchar(36) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`item\` ADD CONSTRAINT \`FK_6b0100c5cb7c67d99ae46197727\` FOREIGN KEY (\`group_id\`) REFERENCES \`group\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`item\` ADD CONSTRAINT \`FK_7c03fe50a5928d547abe334633b\` FOREIGN KEY (\`coupon_id\`) REFERENCES \`coupon\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`coupon\` ADD CONSTRAINT \`FK_a6b17d42c728b530202640aa3d7\` FOREIGN KEY (\`store_id\`) REFERENCES \`store\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`coupon\` ADD CONSTRAINT \`FK_dd06e2d16e22cfa4e6e8f1e82cf\` FOREIGN KEY (\`payment_id\`) REFERENCES \`payment\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`coupon\` DROP FOREIGN KEY \`FK_dd06e2d16e22cfa4e6e8f1e82cf\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`coupon\` DROP FOREIGN KEY \`FK_a6b17d42c728b530202640aa3d7\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`item\` DROP FOREIGN KEY \`FK_7c03fe50a5928d547abe334633b\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`item\` DROP FOREIGN KEY \`FK_6b0100c5cb7c67d99ae46197727\``,
+    );
+    await queryRunner.query(`DROP TABLE \`coupon\``);
+    await queryRunner.query(`DROP TABLE \`payment\``);
+    await queryRunner.query(`DROP TABLE \`item\``);
+    await queryRunner.query(`DROP TABLE \`group\``);
+    await queryRunner.query(`DROP TABLE \`store\``);
+  }
+}
