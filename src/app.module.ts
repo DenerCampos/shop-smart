fd15ba6563be7361c1dd3148e8c05a6492d5ebc9
DenerCampos <denerjcampos@gmail.com>
2026-03-26 16:21:44 -0300

    feat(static-assets): add ServeStaticModule for serving static files and include manifest and icon assets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
 import { TypeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
@@ -23,6 +25,10 @@ import { FamilyGroupModule } from './family-group/family-group.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfig,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public',
     }),
     UserModule,
     AuthModule,
